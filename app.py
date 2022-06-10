7ff83fe18d68db61d8f4829ff484e3a7d7e98d90
Avadhesh Gaur <40993196+its-av@users.noreply.github.com>
2022-06-11 00:03:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,6 @@ if __name__ == '__main__':
     #http_server = WSGIServer(('', 1000), app)
     #http_server.serve_forever()
 
-    http_server = WSGIServer(("0.0.0.0", 5000), app)
-    http_server.serve_forever()
+    #http_server = WSGIServer(("0.0.0.0", 5000), app)
+    #http_server.serve_forever()
     app.run()
